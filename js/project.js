cb1bdad9535d8c2f27a543cf2adbbda58869577e
Beret Parrot <danielspr3@gmail.com>
2015-05-21 23:32:53 +0300

    Severe redesign of all pages. Changed color schema, added animations. Cleaned css.

---
+++ js/project.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.leftPart').ready(function(){
+    $('.leftPart').addClass('animated fadeInLeft');
+  })
+
+  $('.rightPart').ready(function(){
+    $('.rightPart').addClass('animated fadeInRight');
+  })
+
+  $('nav').ready(function(){
+    $('nav').addClass('animated fadeInDown');
+  })
+
+  $('footer').ready(function(){
+    $('footer').addClass('animated fadeInUp');
+  })
+});
