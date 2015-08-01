aaa33b15f9479f966b41f6cb03d4409695efc6fb
Beret Parrot <danielspr3@gmail.com>
2015-08-02 01:43:35 +0300

    Split profile page, created basic page layout.

---
+++ js/profile.js
@@ -0,0 +1,38 @@
+$(".auth").leanModal({top : 100, overlay : 0.6, closeButton: ".modal_close" });
+
+//Auth
+$("#login_form").click(function () {
+    $(".social_login").hide();
+    $(".user_update").hide();
+    $(".user_register").hide();
+    $(".user_login").show();
+    return false;
+});
+
+// Calling Register Form
+$("#register_form").click(function () {
+    $(".social_login").hide();
+    $(".user_update").hide();
+    $(".user_register").show();
+    $(".header_title").text('Register');
+    return false;
+});
+
+// Going back to Social Forms
+$(".back_btn").click(function () {
+    $(".user_login").hide();
+    $(".user_update").hide();
+    $(".user_register").hide();
+    $(".social_login").show();
+    $(".header_title").text('Login');
+    return false;
+});
+
+$('#update_form').click(function(){
+  $(".user_login").hide();
+  $(".user_register").hide();
+  $(".social_login").hide();
+  $(".user_update").show();
+  $(".header_title").text("Refresh");
+  return false;
+});
